46def5e26a3b29e1b626236d7790269f9d510809
bill chipman <wrchipman@gmail.com>
2023-09-27 21:55:45 -0400

    Hike program completed

---
+++ hike.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+
+def display_title():
+    print("Hike Calculator")
+    print()
+
+
+def to_feet(miles):
+    feet = miles * 5280
+    feet = int(round(feet))
+    return feet
+
+
+def main():
+    display_title()
+    miles = float(input("How many miles did you walk?:  "))
+    feet = to_feet(miles)
+    print(f"You walked {feet} feet.")
+
+
+if __name__ == "__main__":
+    main()
