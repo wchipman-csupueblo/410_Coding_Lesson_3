9d83ced7fcb2d209391eab8dd66248203fd71547
bill chipman <wrchipman@gmail.com>
2023-09-27 22:54:00 -0400

    sales tax functions done

---
+++ sales_tax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+TAX_RATE = 0.06
+
+
+def get_tax(total):
+    tax = total * TAX_RATE
+    return round(tax, 2)
+
+
+def get_total_after_tax(total):
+    total_after_tax = total + get_tax(total)
+    return round(total_after_tax, 2)
